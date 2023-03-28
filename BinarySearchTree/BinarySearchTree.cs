fb89483a493fe00242ece5f5f3f5d327fc6d3e4e
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-28 19:44:21 +0530

    [Aniket]- search 63 in BTS

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -9,6 +9,8 @@ namespace BinarySearchTree
     //Creating BST using generics and IComparable to use CompareTo method in program
     class BinarySearchTree<T> where T : IComparable<T>
     {
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
         public T NodeData { get; set; }
         public BinarySearchTree<T> leftTree { get; set; }
         public BinarySearchTree<T> rightTree { get; set; }
@@ -21,9 +23,6 @@ namespace BinarySearchTree
 
         }
 
-        int leftCount = 0, rightCount = 0;
-        bool result = false;
-
         //Method to check node and add node value to either left or right by comparing
         public void Insert(T item)
         {
@@ -31,16 +30,29 @@ namespace BinarySearchTree
             if ((currentNodeValue.CompareTo(item)) > 0)
             {
                 if (this.leftTree == null)
+                {
+                    this.leftCount++;
                     this.leftTree = new BinarySearchTree<T>(item);
+                }
                 else
+                {
+
+                    this.leftCount++;
                     this.leftTree.Insert(item);
+                }
             }
             else
             {
                 if (this.rightTree == null)
+                {
+                    this.rightCount++;
                     this.rightTree = new BinarySearchTree<T>(item);
+                }
                 else
+                {
+                    this.rightCount++;
                     this.rightTree.Insert(item);
+                }
             }
         }
 
@@ -49,14 +61,11 @@ namespace BinarySearchTree
         {
             if (this.leftTree != null)
             {
-                this.leftCount++;
                 this.leftTree.Display();
-
             }
             Console.WriteLine(this.NodeData.ToString());
             if (this.rightTree != null)
             {
-                this.rightCount++;
                 this.rightTree.Display();
             }
         }
@@ -67,5 +76,23 @@ namespace BinarySearchTree
             Console.WriteLine("Size of Binary Tree " + " " + (1 + this.leftCount + this.rightCount));
         }
 
+        //Method tto search the given value in the binary tree
+        public bool IfExists(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+                return false;
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                result = true;
+            }
+            else
+                Console.WriteLine("Current element is {0} in BST", node.NodeData);
+            if (element.CompareTo(node.NodeData) < 0)
+                IfExists(element, node.leftTree);
+            if (element.CompareTo(node.NodeData) > 0)
+                IfExists(element, node.rightTree);
+            return result;
+        }
     }
 }
